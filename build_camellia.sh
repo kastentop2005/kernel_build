9c72a1de4bd17ee6b38c1992e9ee5c6e540ffaa3
Konnlori <kastentop2005@gmail.com>
2023-07-31 20:15:22 +0300

    build_camellia.sh: revamp

--- build_camellia.sh
+++ build_camellia.sh
@@ -5,41 +5,71 @@
 
 ###### Initial configuration begin ######
 # Colors
-Red=$'\e[1;31m'
-Green=$'\e[1;32m'
-Blue=$'\e[1;34m'
+normal='\033[0m'
+red='\033[0;31m'
+green='\033[0;32m'
+orange='\033[0;33m'
+blue='\033[0;34m'
+purple='\033[0;35m'
+cyan='\033[0;36m'
+lightred='\033[1;31m'
+lightgreen='\033[1;32m'
+lightblue='\033[1;34m'
+lightpurple='\033[1;35m'
+lightcyan='\033[1;36m'
+white='\033[1;37m'
+
 # Variables
 SECONDS=0 # Built-in bash timer
-zipname="OSS-Camellia-$(date '+%Y%m%d-%H%M').zip" # How to name the zip (Name + date)
+zipname="OSS-Camellia-$(date '+%Y%m%d-%H%M').zip" # Zip name (Name + date)
 tc="$HOME/tc/proton-clang" # Toolchain path
-defconfig="camellia_defconfig" # Which defconfig to use
+defconfig="camellia_defconfig" # Defconfig to use
+logfile="$HOME/build.log"
+
+# Exports
 export PATH="$tc/bin:$PATH" # Expose toolchain path to system
-# Extra
-anykernel=1 # 0 - do not use AnyKernel; 1 - use AnyKernel
 ###### Initial configuration end ######
+
+# make clean
+if [[ $1 = "-c" || $1 = "--clean" ]]; then
+    make clean
+fi
+
+# Logging
+if [[ $2 = -l || $2 = --log ]]; then
+    if [[ -f $HOME/build.log ]]; then
+        rm -rf $HOME/build.log
+    fi
+    exec > >(tee $HOME/build.log)
+fi
 
 ###### Toolchain setup begin ######
 toolchain_setup () {
 if ! [ -d "$tc" ]; then
-echo "$Blue Toolchain was not found. Cloning..."
-if ! git clone --depth=1 --single-branch https://github.com/kdrag0n/proton-clang $tc; then
-echo "$Red Cloning toolchain has failed! Aborting..."
-exit 1
+printf "${lighblue} Toolchain was not found. Cloning...${normal}\n"
+    if ! git clone --depth=1 --single-branch https://github.com/kdrag0n/proton-clang $tc; then
+    printf "${lightred} Cloning toolchain has failed! Aborting...${normal}\n"
+    exit 1
+    fi
 fi
-fi
-}
-###### Toolchain setup end ######
 
-# make clean
-if [[ $1 = "-c" || $1 = "--clean" ]]; then
-rm -rf out
-fi
+# Check for dependencies
+for dep in lld bc
+do
+    if ! command -v $dep; then
+    printf "Installing $dep...\n"
+        sudo apt install -y $dep;
+    fi
+done
+}
+
+###### Toolchain setup end ######
 
 ###### Build begin ######
 build () {
 mkdir -p out
 make O=out ARCH=arm64 $defconfig
-echo -e "$Blue\nStarting compilation...\n"
+printf "${lightblue}Starting compilation...{$normal}\n"
 make -j$(nproc --all) \
                     O=out \
                     ARCH=arm64 \
@@ -50,12 +80,14 @@ make -j$(nproc --all) \
                     OBJCOPY=llvm-objcopy \
                     OBJDUMP=llvm-objdump \
                     STRIP=llvm-strip \
-                    CROSS_COMPILE=aarch64-linux-gnu- Image.gz dtbo.img
+                    CROSS_COMPILE=aarch64-linux-gnu- Image.gz-dtb \
+
 # Compilation info
 if [ -f "out/arch/arm64/boot/Image.gz" ]; then
-    echo -e "$Green\nBuild has been succesfully completed in $((SECONDS / 60)):$((SECONDS % 60))"
+    printf "${lightgreen}Build has been succesfully completed in $((SECONDS / 60)):$((SECONDS % 60))${normal}"
 else
-    echo -e "$Red\nCompilation failed!"
+    printf "${ightred}Compilation has failed!${normal}"
+    exit 1
 fi
 }
 ###### Build end ######
@@ -63,14 +95,16 @@ fi
 ###### AnyKernel begin ######
 anykernel () {
 if ! [ -f "out/arch/arm64/boot/Image.gz" ]; then
-echo -e "\nNo image to work with, aborting!"
+printf "${lightred}No image to work with, aborting...${normal}"
 rm -rf AnyKernel3
-exit
-fi
-if ! git clone https://github.com/kastentop2005/AnyKernel3; then
-echo -e "$Red\nCloning AnyKernel3 repo has failed! Aborting..."
 exit 1
 fi
+
+if ! git clone https://github.com/kastentop2005/AnyKernel3; then
+printf "${lightred}Cloning AnyKernel3 repo has failed! Aborting...${normal}"
+exit 1
+fi
+
 cp out/arch/arm64/boot/Image.gz AnyKernel3
 rm -f *zip
 cd AnyKernel3
@@ -78,15 +112,16 @@ rm -rf out/arch/arm64/boot
 zip -r9 "../$zipname" * -x '*.git*' README.md *placeholder
 cd ..
 rm -rf AnyKernel3
-echo "$Green Zip has been saved as: $zipname"
+printf "${lightgreen}Zip has been saved as: $zipname${normal}"
 }
 ###### Anykernel end ######
 
 ###### Run begin ######
 toolchain_setup
 build
+
 # Check if anykernel needs to be used
-if [ "$anykernel" -eq 1 ]; then
-    anykernel;
+if [[ $3 = "-ak" || $3 = "--anykernel" ]]; then
+    anykernel
 fi
 ###### Run end ######
